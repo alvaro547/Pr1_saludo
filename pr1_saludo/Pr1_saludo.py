e184e7dc866d00d3996a372c22fb220feec2ce7a
alvaro547 <a.23308051281175@cbtis128.edu.mx>
2024-10-08 20:50:43 -0600

    Add files via upload

---
+++ pr1_saludo/Pr1_saludo.py
@@ -0,0 +1,14 @@
+print(" ")
+print("Alvaro Campechano 3W")
+print(" ")
+def saludar():
+    """
+    Esta función imprime el saludo "Hey amigos!" en la consola.
+    
+    No recibe parámetros y no devuelve ningún valor.
+    """
+    print("Hey amigos!")
+
+# Ejemplo de uso de la función
+if __name__ == "__main__":
+    saludar()  # Llama a la función para mostrar el saludo
